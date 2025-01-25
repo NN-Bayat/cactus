0ce40c5311aa26ac10e7335b657d3289ff586be8
NN-Bayat <noorullahbayat1414@gmail.com>
2025-01-25 17:35:29 +0430

    js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,32 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
 
+// Show Menu
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    });
+}
+
+// Hide Menu
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLinks = document.querySelectorAll('.nav__links');
 
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu');
+
+    navMenu.classList.remove('show-menu');
+}
+
+navLinks.forEach(n => n.addEventListener('click', linkAction));
 
 /*=============== ADD BLUR HEADER ===============*/
 
