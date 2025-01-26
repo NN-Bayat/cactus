4ad281b88f9d6c88390fa7b7b511d16adc6c9c59
NN-Bayat <noorullahbayat1414@gmail.com>
2025-01-26 13:53:14 +0430

    js file added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,92 @@
 /*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu'),
-    navToggle = document.getElementById('nav-toggle'),
-    navClose = document.getElementById('nav-close');
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
 // Show Menu
 if (navToggle) {
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu');
-    });
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
 }
 
 // Hide Menu
 if (navClose) {
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu');
-    });
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
-const navLinks = document.querySelectorAll('.nav__links');
+const navLinks = document.querySelectorAll(".nav__links");
 
 const linkAction = () => {
-    const navMenu = document.getElementById('nav-menu');
+  const navMenu = document.getElementById("nav-menu");
 
-    navMenu.classList.remove('show-menu');
-}
+  navMenu.classList.remove("show-menu");
+};
 
-navLinks.forEach(n => n.addEventListener('click', linkAction));
+navLinks.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== ADD BLUR HEADER ===============*/
 const blurHeader = () => {
-    const header = document.getElementById('header');
+  const header = document.getElementById("header");
 
-    this.scrollY >= 50 ? header.classList.add('blur-header')
-        : header.classList.remove('blur-header')
-}
-window.addEventListener('scroll', blurHeader);
+  this.scrollY >= 50
+    ? header.classList.add("blur-header")
+    : header.classList.remove("blur-header");
+};
+window.addEventListener("scroll", blurHeader);
 
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
 
+  this.scrollY >= 150
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
 
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
+
+    if (sectionsClass) {
+      if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+        sectionsClass.classList.add("active-link");
+      } else {
+        sectionsClass.classList.remove("active-link");
+      }
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
+/*=============================*/
+document.querySelectorAll(".contact__social a i").forEach((icon) => {
+  icon.onclick = (e) => {
+    e.preventDefault();
+    alert("I'm not using any of those platforms! 😐");
+    alert("Try another way 🤙");
+  };
+});
+
+document.querySelectorAll(".shop__button").forEach((button) => {
+  button.onclick = (e) => {
+    e.preventDefault();
+    alert("There is not any item! 😕");
+  };
+});
